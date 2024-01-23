51296f015217fef3ecc28ba4da50a4d20fdda77e
Josué Klaysler <josueklayslerjk@gmail.com>
2024-01-23 20:41:29 -0200

    Aprendendo  variavel

---
+++ MeusProjetos_Python/Cap2-Variaveis/Variaveis2.py
@@ -0,0 +1,8 @@
+nome=input('Seu nome: ')
+evento=input('Nome do evento:')
+Va_entrada=float(input('Val0r de entrada:'))
+
+
+
+
+print(f'Declaro que o senhor Gonçalves Dias que o {nome} esteve presente no {evento} evento e gastou o valor de {Va_entrada} com a entrada.')
