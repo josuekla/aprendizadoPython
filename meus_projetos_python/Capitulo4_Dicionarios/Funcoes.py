9342791004699cd5034310dde37d07c6c9586485
Josué Klaysler <josueklayslerjk@gmail.com>
2024-02-01 09:16:54 -0200

    Criando funções de pesquisar,excluir e listar

--- meus_projetos_python/Capitulo4_Dicionarios/Funcoes.py
+++ meus_projetos_python/Capitulo4_Dicionarios/Funcoes.py
@@ -11,3 +11,21 @@ def inserir(dicionario):
     dicionario[input("Digite o login: ").upper()] = [input("Digite o nome: ").upper(),
                                                      input("Digite a última data de acesso: "),
                                                      input("Qual a última estação acessada:").upper()]
+
+
+def pesquisar(dicionario, chave):
+    lista = dicionario.get(chave)
+    if lista is not None:
+        print("O nome é: ", lista[0])
+        print("Última data de acesso: ", lista[1])
+        print("Última estação: ", lista[2])
+
+def excluir(dicionario, chave):
+    if dicionario.get(chave) is not None:
+        del dicionario.[chave]
+
+def listar(dicionario):
+    for chave,valor in dicionario.item():
+        print("OBJETO.........")
+        print("Login: ",chave)
+        print("Dados: ",valor)
