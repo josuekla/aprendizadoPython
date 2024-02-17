bef59e18e040ba3adba257d82bb3f4513447826f
Josué Klaysler <josueklayslerjk@gmail.com>
2024-02-17 16:02:01 -0200

    Criando um conversor de moeda

---
+++ exercíciosdepython/Exercicios de python/ex010.py
@@ -0,0 +1,6 @@
+money = float(input("Quando dinheiro voçê tem na carteira? R$"))
+usd = money / 4.97
+euro = money / 5.35
+
+print(f"\nCom R${money} voçê pode comprar {usd:.2f} dólares atualmente(17/02/24)\n")
+print(f"\nCom R${money} voçê pode comprar {euro:.2f} euros atualmente(17/02/24)\n")
